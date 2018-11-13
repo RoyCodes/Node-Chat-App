46a5c1b8d8bad8751f50e8b710a839e8d0c22919
Roy Kincaid <roy@kincaid.ninja>
2018-11-13 07:05:40 -0800

    Added form for posting messages and show new messages

--- server/server.js
+++ server/server.js
@@ -21,9 +21,10 @@ io.on('connection', (socket) => {
     // socket.broadcast.emit from Admin "New User Joined"
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
     });
 
     socket.on('disconnect', (socket) => {
